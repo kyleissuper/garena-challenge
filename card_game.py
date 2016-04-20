aeed1f482c9d86731fd8b0a6691cb67d360a80c0
Kyle Tan <kyle@kyletan.com>
2016-04-21 01:26:42 +0800

    Changed variable names for understandability

--- card_game.py
+++ card_game.py
@@ -4,55 +4,55 @@ import random
 card_map = {
         "A": {
             "card": "Ace of Spades",
-            "value": 0
+            "id": 0
             },
         "B": {
             "card": "King of Spades",
-            "value": 1
+            "id": 1
             },
         "C": {
             "card": "Queen of Spades",
-            "value": 2
+            "id": 2
             },
         "D": {
             "card": "Jack of Spades",
-            "value": 3
+            "id": 3
             },
         "E": {
             "card": "10 of Spades",
-            "value": 4
+            "id": 4
             },
         "F": {
             "card": "9 of Spades",
-            "value": 5
+            "id": 5
             },
         "G": {
             "card": "8 of Spades",
-            "value": 6
+            "id": 6
             },
         "H": {
             "card": "7 of Spades",
-            "value": 7
+            "id": 7
             },
         "I": {
             "card": "6 of Spades",
-            "value": 8
+            "id": 8
             },
         "J": {
             "card": "5 of Spades",
-            "value": 9
+            "id": 9
             },
         "K": {
             "card": "4 of Spades",
-            "value": 10
+            "id": 10
             },
         "L": {
             "card": "3 of Spades",
-            "value": 11
+            "id": 11
             },
         "M": {
             "card": "Deuce of Spades",
-            "value": 12
+            "id": 12
             }
         }
 
@@ -66,19 +66,19 @@ class Game:
 
     def play(self, cards=cards):
         "Run game once"
-        self.arrangement = cards
-        for player_id, letter in enumerate(self.arrangement):
+        self.cards = cards
+        for player_id, letter in enumerate(self.cards):
             successful = False
             attempts = 0
             while not successful:
                 current_card = self.card_map[letter]
-                if player_id == current_card["value"]:
+                if player_id == current_card["id"]:
                     successful = True
-                letter = self.arrangement[current_card["value"]]
+                letter = self.cards[current_card["id"]]
                 attempts += 1
                 if attempts > self.max_attempts:
                     return "Game over. Failed."
-        return "Success"
+        return "Success!"
 
 G = Game()
 
@@ -87,7 +87,7 @@ rounds = 50000
 win_rate = 0
 for i in range(rounds):
     random.shuffle(cards) # important!
-    if G.play(cards) == "Success":
+    if G.play(cards) == "Success!":
         win_rate += 1/rounds
 win_rate = round(win_rate * 100, 1)
 print "The strategy won {}% of {} rounds.".format(win_rate, rounds)
